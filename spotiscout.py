562d4620a2dde9af8d6233f4329b30a9f4c60844
macow-lab <mmacovv@gmail.com>
2021-07-23 20:36:37 +0200

    Plate

---
+++ spotiscout.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, Response
+
+app = Flask(__name__)
+
+
+database = Database()
+fake = Faker()
+
+
+@app.route("/")
+def method():
+    return NotImplemented
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
