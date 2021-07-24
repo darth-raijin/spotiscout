3142c1852ec12bd6d1e14723b744b9f331433c81
macow-lab <mmacovv@gmail.com>
2021-07-25 00:45:29 +0200

    Setup of Spotipy and OAuth started

--- spotiscout.py
+++ spotiscout.py
@@ -1,11 +1,20 @@
-from flask import Flask, request, render_template, Response
+from dotenv import load_dotenv
+import os
+from flask import Flask, request, render_template, url_for, session, redirect
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 from tracks import tracks
 from albums import albums
 from genres import genres
 from recent import recent
 
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = NotImplemented
+app.config['SESSION_COOKIE_NAME'] = NotImplemented
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
 
 # Registering blueprints
 app.register_blueprint(tracks)
@@ -18,10 +27,33 @@ app.register_blueprint(recent)
 def root():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    spotify_oauth = create_spotify_oauth() 
+    spotify_auth_url = spotify_oauth.get_authorize_url()
+    return redirect(spotify_auth_url)
+
+@app.route("/logout")
+def logout():
+    return render_template("index.html")
+
+@app.route("/redirect")
+def redirect():
+    print("Redirect received!")
+    return render_template("index.html")
+
 @app.route("/settings")
 def settings():
     return render_template("index.html")
 
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id = os.getenv('CLIENT_ID'),
+        client_secret = os.getenv('CLIENT_SECRET'),
+        redirect_uri = url_for("redirect", _external=True),
+        scope="user-library-read"
+)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
