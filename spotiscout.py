ea438ededc8bb04240e37cdec56316f6576eccd8
macow-lab <mmacovv@gmail.com>
2021-07-25 02:47:19 +0200

    OAuth Progress

--- spotiscout.py
+++ spotiscout.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, request, render_template, url_for, session, redirect
 import spotipy
+import time
 from spotipy.oauth2 import SpotifyOAuth
 from tracks import tracks
 from albums import albums
@@ -38,8 +39,13 @@ def logout():
     return render_template("index.html")
 
 @app.route("/redirect")
-def redirect():
+def auth_receiver():
     print("Redirect received!")
+    spotify_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = spotify_oauth.get_access_token(code)
+    session["token_info"] = token_info
     return render_template("index.html")
 
 @app.route("/settings")
@@ -51,9 +57,28 @@ def create_spotify_oauth():
     return SpotifyOAuth(
         client_id = os.getenv('CLIENT_ID'),
         client_secret = os.getenv('CLIENT_SECRET'),
-        redirect_uri = url_for("redirect", _external=True),
+        redirect_uri = url_for("auth_receiver", _external=True),
         scope="user-library-read"
 )
+
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Check if session token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    if (is_token_expired):
+        spotify_oauth = create_spotify_oauth()
+        token_info = spotify_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
