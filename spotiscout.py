36a864f6e98f27ad73ec3240c31a11647c5dfe82
macow-lab <mmacovv@gmail.com>
2021-07-26 14:15:28 +0200

    Progress on endpoints, additions to scope

--- spotiscout.py
+++ spotiscout.py
@@ -2,23 +2,19 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, request, render_template, url_for, session, redirect
 import spotipy
-import token_auth
 import time
 from spotipy.oauth2 import SpotifyOAuth
-from tracks import tracks
-from albums import albums
-from genres import genres
-from recent import recent
+
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = NotImplemented
-app.config['SESSION_COOKIE_NAME'] = NotImplemented
+app.secret_key =  "Doflamingo"
+app.config['SESSION_COOKIE_NAME'] = "Quijote"
 
 @app.route("/")
 def root():
-    return render_template("index.html" )
+    return render_template("index.html")
     # TODO Pass data, based on token validity, call confirm_auth()
 
 @app.route("/login")
@@ -29,7 +25,40 @@ def login():
 
 @app.route("/logout")
 def logout():
-    return render_template("index.html")
+    session.clear()
+    return redirect(url_for("root"))
+
+@app.route("/profile")
+def user_profile():
+    return render_template("profile.html")
+
+@app.context_processor
+def is_authenticated():
+    return dict(is_auth = confirm_auth())
+
+@app.route("/session")
+def get_session():
+    print(session)
+    return "hell"
+
+@app.route("/me")
+def get_user_profile():
+    confirm_auth()
+    sp = spotipy.Spotify(auth = session.get("token_info").get("access_token"))
+    # TODO Create profile, based on current_user()
+    # Pass External URL (Spotify), image URL, and Display Name to Template
+    
+    spotify_profile = sp.current_user()
+    profile_dict = {
+        "display_name": spotify_profile["display_name"],
+        "spotify_url": spotify_profile["external_urls"]["spotify"],
+        "follower_total": spotify_profile["followers"]["total"],
+        "image_url": spotify_profile["images"][0]["url"],
+        "top_artist": None
+    }
+
+    session["user_info"] = profile_dict
+    return session["user_info"]
 
 @app.route("/redirect")
 def auth_receiver():
@@ -39,7 +68,7 @@ def auth_receiver():
     code = request.args.get('code')
     token_info = spotify_oauth.get_access_token(code)
     session["token_info"] = token_info
-    return render_template("index.html")
+    return redirect(url_for("root"))
 
 @app.route("/settings")
 def settings():
@@ -47,24 +76,41 @@ def settings():
 
 
 # TRACKS API
-@app.route('tracks/top', defaults = {'range': 'all_time'})
+@app.route('/tracks/top', defaults = {'range': 'all_time'})
 def top_tracks(range):
-    return render_template("tracks.html")
+    confirm_auth()
+    spotify = spotipy.Spotify(auth = session.get("token_info").get("access_token"))
+
+    results = []
+    iter = 0
+    while True:
+        offset = iter * 50
+        iter += 1
+        curGroup = spotify.current_user_saved_tracks(limit=50, offset=offset)['items']
+        for idx, item in enumerate(curGroup):
+            track = item['track']
+            val = track['name'] + " - " + track['artists'][0]['name']
+            results += [val]
+        if (len(curGroup) < 50):
+            break
+
+
+    return spotify.current_user_saved_tracks(limit=50, offset=offset)['items']
 
 # GENRES API
-@app.route('genres/top', defaults = {'range': 'all_time'})
+@app.route('/genres/top', defaults = {'range': 'all_time'})
 def top_genres(range):
-    return NotImplemented
+    return render_template("genres.html")
 
 # RECENT API
-@app.route('recent/', defaults = {'item': 'tracks'})
+@app.route('/recent/', defaults = {'item': 'tracks'})
 def recent(item):
-    return item
+    return render_template("recent.html")
 
 # ALBUMS API
-@app.route('albums/top', defaults = {'range': 'alltime'})
+@app.route('/albums/top', defaults = {'range': 'alltime'})
 def top_albums(range):
-    return NotImplemented
+    return render_template("album.html")
 
 
 def create_spotify_oauth():
@@ -72,14 +118,14 @@ def create_spotify_oauth():
         client_id = os.getenv('CLIENT_ID'),
         client_secret = os.getenv('CLIENT_SECRET'),
         redirect_uri = url_for("auth_receiver", _external=True),
-        scope="user-library-read"
+        scope="user-library-read user-read-recently-played user-top-read playlist-modify-public"
 )
 
 def confirm_auth():
     session['token_info'], authorized = get_token()
     session.modified = True
     if not authorized:
-        return redirect(url_for("root"))
+        return False
     return True
 
 def get_token():
